13ab11e7fc109b0634b120300f37293d4a8edd57
Estel Seong <estel.castle@gmail.com>
2016-05-30 22:57:25 +0900

    ruby... db 연결 및 생성에서 안된다...

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,2 +1,23 @@
-# initialize application
 require 'sinatra'
+require 'json'
+require 'mongo'
+
+set :bind, '0.0.0.0'
+
+db_ip = [ENV['DB_1_PORT_27017_TCP_ADDR']]
+client = Mongo::Client.new(['192.168.99.100:27017'], database: 'blog')
+
+get '/' do
+  @posts = client[:posts].find.to_a
+  erb :blog
+end
+
+get '/json' do
+  content_type :json
+  client[:posts].find.to_a.to_json
+end
+
+post '/new' do
+  client[:posts].insert_one(params)
+  redirect to('/')
+end
