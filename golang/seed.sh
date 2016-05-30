56bf6b960ef53b8a9392e38823e7dbd3cdfd228f
Estel Seong <estel.castle@gmail.com>
2016-05-30 23:13:40 +0900

    go go...

---
+++ golang/seed.sh
@@ -0,0 +1,17 @@
+DOCKER_IP=$(docker-machine ip)
+SERVICE_PORT=3000
+
+curl --form title='Test Post 4' \
+     --form summary='The Fourth Test Post' \
+     --form content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' \
+     http://$DOCKER_IP:$SERVICE_PORT/new
+
+curl --form title='Test Post 5' \
+     --form summary='The Fifth Test Post' \
+     --form content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' \
+     http://$DOCKER_IP:$SERVICE_PORT/new
+
+curl --form title='Test Post 6' \
+     --form summary='The Sixth Test Post' \
+     --form content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' \
+     http://$DOCKER_IP:$SERVICE_PORT/new
